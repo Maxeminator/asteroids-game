57d1945eb58cbeecac3a4c8ff600d30bd1cf160a
Maxeminator <kil201@mail.ru>
2025-04-22 13:29:58 +0300

    Game Logic with Asteroids and movement

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pygame
 from constants import *
+from circleshape import*
+from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     print("Starting Asteroids!")
@@ -7,15 +11,36 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
-    screen= pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    
+    dt=0
 
+    screen= pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    
+    updatable=pygame.sprite.Group()
+    drawable=pygame.sprite.Group()
+    asteroids=pygame.sprite.Group()
+    AsteroidField.containers=(updatable)
+    Asteroid.containers=(asteroids,updatable,drawable)
+    Player.containers=(updatable,drawable)
+    asteroid=Asteroid(SCREEN_WIDTH,SCREEN_HEIGHT,2)
+    player=Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
+    
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        
+        updatable.update(dt)
 
         screen.fill((0,0,0,))
+
+        for draw in drawable:
+            draw.draw(screen)
+
+        dt = clock.tick(60)/1000
 
         pygame.display.flip()
     pygame.quit()
